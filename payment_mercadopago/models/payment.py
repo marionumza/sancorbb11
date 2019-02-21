1e7ce5caaef71c8078d2ede1a6a3998b57673e29
Jesus Rabelo <rabeloj34@gmail.com>
2019-02-21 14:04:24 -0500

    Debug payments searchs

--- payment_mercadopago/models/payment.py
+++ payment_mercadopago/models/payment.py
@@ -565,7 +565,7 @@ class PaymentTransactionMercadoPago(models.Model):
     def _cron_recover_abandoned_payment_mercadopago(self):
         _logger.info("Checking for Abandoned Payments from MercadoPago. Trying to recover Payment Transactions.")
 
-        olddays = datetime.strptime(str(datetime.now()), DATE_FORMAT) - timedelta(days=7)
+        olddays = datetime.strptime(str(datetime.now().date()), DATE_FORMAT) - timedelta(days=7)
 
         transactions = self.env['payment.transaction'].sudo().search([('provider', '=', 'mercadopago'),
                                                                             ('state', 'in', ['draft']),
@@ -595,12 +595,12 @@ class PaymentTransactionMercadoPago(models.Model):
         else:
             _logger.info("No Abandoned transaction found against MercadoPago Payment Gateway.")
 
-        olddays = datetime.strptime(str(datetime.now()), DATE_FORMAT) - timedelta(days=3)
+        oldday = datetime.strptime(str(datetime.now().date()), DATE_FORMAT) - timedelta(days=3)
 
         transactions_cancel = self.env['payment.transaction'].sudo().search(
             [('provider', '=', 'mercadopago'),
              ('state', 'in', ['cancel']),
-             ('create_date', '>=', olddays.strftime(DATETIME_FORMAT)),
+             ('create_date', '>=', oldday.strftime(DATETIME_FORMAT)),
              ('acquirer_reference', '=', False)])
 
         if transactions_cancel:
