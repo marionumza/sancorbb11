8586b05fd1f1446aac5b42be2b832e03fb2af0a6
yaritza91 <yariyanet91@gmail.com>
2019-03-22 13:00:56 -0300

    estilo popup

--- website_mass_mailing_extends/__manifest__.py
+++ website_mass_mailing_extends/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','website','website_mass_mailing','website_sale'],
+    'depends': ['base','website_mass_mailing','mass_mailing'],
 
     # always loaded
     'data': [
